a05080ba5c7b11c88f7fd4eba97bf1c0d4d1a397
Michael-Whittington <107943021+Michael-Whittington@users.noreply.github.com>
2023-09-21 13:35:46 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -11,8 +11,6 @@ def create_service_context():
     #constraint parameters
     max_input_size = 4096
     num_outputs = 512
-    #max_chunk_overlap = 20
-    #chunk_size_limit = 600
     #allows the user to explicitly set certain constraint parameters
     prompt_helper = PromptHelper(max_input_size, num_outputs,  chunk_overlap_ratio=0.2)
     #LLMPredictor is a wrapper class around LangChain's LLMChain that allows easy integration into LlamaIndex
@@ -32,20 +30,29 @@ def data_ingestion_indexing(directory_path):
     #persist index to disk, default "storage" folder
     index.storage_context.persist()
     return index
+    
 def data_querying(input_text):
+    #split the input text by question mark and strip any leading/trailing spaces
+    input_texts = [q.strip() for q in input_text.split("?") if q.strip()]
     #rebuild storage context
     storage_context = StorageContext.from_defaults(persist_dir="/workspaces/PDF_Chatbot/storage")
     #loads index from storage
     index = load_index_from_storage(storage_context, service_context=create_service_context())
+    # queries the index with each input text
+    results = []
+    for input_text in input_texts:
+        #queries the index with the input text
+        response = index.as_query_engine().query(input_text)
+        doc_id = response.get_formatted_sources()
+        results.append({"response": response.response, "information_origin": doc_id})
     
-    #queries the index with the input text
-    response = index.as_query_engine().query(input_text)
-    doc_id = response.get_formatted_sources()
-    #return response.response
-    return {"response": response.response, "information_origin": doc_id}
+    combined_results = "\n\n".join(
+        [f"Q: {input_texts[i]}\nA: {res['response']}\nSource: {res['information_origin']}" for i, res in enumerate(results)]
+    )
+    return combined_results
 
 iface = gr.Interface(fn=data_querying,
-                     inputs=gr.components.Textbox(lines=7, label="Enter your question"),
+                     inputs=gr.components.Textbox(lines=7, label="Enter your questions separated by a question mark"),
                      outputs="text",
                      title="Q and A Bot")
 #passes in data directory
